da06b47d24e4b6b8d73b4c45feafa7256fcaf2e2
LuigiSouza <lpsouza@inf.ufsm.br>
2022-06-16 22:01:57 -0300

    minor chages to help in debug and javac

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -6,9 +6,13 @@ import User.UserChat;
 public class ChatClient {
     public static void main(String[] args) {
         try {
-            IServerChat srv = (IServerChat) Naming.lookup("//localhost:2020/Server");
+            int port = 2020;
+
+            System.out.println("Connecting to server...");
+            IServerChat srv = (IServerChat) Naming.lookup("rmi://localhost:" + port + "/Server");
             UserChat usr = new UserChat(srv);
-        } catch(Exception e) {
+            System.out.println("Connected to server on port " + port + "...");
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,13 +1,21 @@
 import java.rmi.registry.LocateRegistry;
+import java.rmi.Naming;
 
+import Server.IServerChat;
 import Server.ServerChat;
 
 public class ChatServer {
     public static void main(String[] args) {
         try {
-            ServerChat srv = new ServerChat();
-            LocateRegistry.createRegistry(2020).rebind("Server", srv);
-        } catch(Exception e) {
+            int port = 2020;
+            IServerChat srv = new ServerChat();
+            String objName = "rmi://localhost:" + port + "/Server";
+
+            System.out.println("Registering server...");
+            LocateRegistry.createRegistry(port).rebind("Server", srv);
+            Naming.rebind(objName, srv);
+            System.out.println("Server is running on port " + port + "...");
+        } catch (Exception e) {
             System.out.println("Server Error: " + e.getMessage());
             e.printStackTrace();
         }
